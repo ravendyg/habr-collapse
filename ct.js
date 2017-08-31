089a15579c1014bb7ee0f3d36b9670afeb586d87
Yaroslav Dashkevich <jdashkevich@xiag.ch>
2017-08-31 13:03:48 +0700

    Implement

--- ct.js
+++ ct.js
@@ -3,10 +3,14 @@
 const
   collapseRegexp = /collapse-control/,
   collapsedRegexp = /collapsed/,
-  defaultClass = 'collapse-control inline-list__item inline-list__item_comment-nav js-comment_children'
+  defaultClass = 'collapse-control inline-list__item inline-list__item_comment-nav js-comment_children',
+  collapseLiColor = 'red',
+  expandLiColor = 'green',
+  collapseLiText = 'Collapse',
+  expandLiText = 'Expand'
   ;
 
-if (['habrahabr.ru', 'geektimes.ru'].indexOf(location.host) !==  1) {
+if (['habrahabr.ru', 'geektimes.ru'].indexOf(location.host) !==  -1) {
   addButtons();
   document.addEventListener('click', clickHandler);
 }
@@ -15,37 +19,52 @@ if (['habrahabr.ru', 'geektimes.ru'].indexOf(location.host) !==  1) {
  * @param {MouseEvent} ev
  */
 function clickHandler(ev) {
+
   const elem = ev.target;
   if (!elem) return;
 
   const klass = elem.getAttribute('class') || '';
-  const list = elem.parent;
-console.log(list)
 
   if (collapseRegexp.test(klass)) {
-    let newClass, visibility;
+
+    const root = elem.parentElement.parentElement.parentElement;
+    const list = root.nextElementSibling;
+    let newClass, display, text, color;
+
     if (!collapsedRegexp.test(klass)) {
       newClass = defaultClass + 'collapsed';
-      visibility = 'hidden';
+      display = 'none';
+      text = expandLiText;
+      color = expandLiColor;
     } else {
       newClass = defaultClass;
-      visibility = 'visible';
+      display = '';
+      text = collapseLiText;
+      color = collapseLiColor;
     }
-    elem.setAttribute('class', newClass);
 
+    elem.setAttribute('class', newClass);
+    list.style.display = display;
+    elem.textContent = text;
+    elem.style.color = color;
 
   }
+
 }
 
 function addButtons() {
+
   const commentControlsHolders = document.querySelectorAll('.inline-list.inline-list_comment-nav') || [];
+
   for (let i = 0; i < commentControlsHolders.length; i++) {
     const holder = commentControlsHolders[i];
     const li = document.createElement('li');
     li.setAttribute('class', defaultClass);
-    li.textContent = 'Collapse';
+    li.textContent = collapseLiText;
     li.style.cursor = 'pointer';
     li.style.marginTop = '5px';
+    li.style.color = collapseLiColor;
     holder.appendChild(li);
   }
+
 }

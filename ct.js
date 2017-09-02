490c5c7a6df6004631f979cd80d0ba249620b97d
Yaroslav <jaroslav.dashkevich@gmail.com>
2017-09-02 17:26:57 +0700

    Add scroll to root comment btn

--- ct.js
+++ ct.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const
-  collapseRegexp = /collapse-control/,
+  collapseBaseClass = 'collapse-control',
+  showRootBaseClass = 'show-root-control',
+  collapseRegexp = new RegExp(collapseBaseClass),
+  showRootRegexp = new RegExp(showRootBaseClass),
   collapsedRegexp = /collapsed/,
-  defaultClass = 'collapse-control inline-list__item inline-list__item_comment-nav js-comment_children',
+  toggleCollapsedDefaultClass = `${collapseBaseClass} inline-list__item inline-list__item_comment-nav js-comment_children`,
+  showRootClass = `${showRootBaseClass} inline-list__item inline-list__item_comment-nav js-comment_children`,
+  commentSectionClass = 'comments-section',
   collapseLiColor = 'red',
   expandLiColor = 'green',
   collapseLiText = 'Collapse',
-  expandLiText = 'Expand'
+  expandLiText = 'Expand',
+  showRootText = 'Root',
+  showRoorColor = 'blue'
   ;
 
 if (['habrahabr.ru', 'geektimes.ru'].indexOf(location.host) !==  -1) {
@@ -26,45 +33,82 @@ function clickHandler(ev) {
   const klass = elem.getAttribute('class') || '';
 
   if (collapseRegexp.test(klass)) {
-
-    const root = elem.parentElement.parentElement.parentElement;
-    const list = root.nextElementSibling;
-    let newClass, display, text, color;
-
-    if (!collapsedRegexp.test(klass)) {
-      newClass = defaultClass + 'collapsed';
-      display = 'none';
-      text = expandLiText;
-      color = expandLiColor;
-    } else {
-      newClass = defaultClass;
-      display = '';
-      text = collapseLiText;
-      color = collapseLiColor;
-    }
-
-    elem.setAttribute('class', newClass);
-    list.style.display = display;
-    elem.textContent = text;
-    elem.style.color = color;
-
+    toggleCollapsed(elem, klass);
+  } else if (showRootRegexp.test(klass)) {
+    showRoot(elem);
   }
 
 }
 
+/**
+ * @param {HTMLElement} elem
+ * @param {string} klass
+ */
+function toggleCollapsed(elem, klass) {
+  const root = elem.parentElement.parentElement.parentElement;
+  const list = root.nextElementSibling;
+  let newClass, display, text, color;
+
+  if (!collapsedRegexp.test(klass)) {
+    newClass = toggleCollapsedDefaultClass + 'collapsed';
+    display = 'none';
+    text = expandLiText;
+    color = expandLiColor;
+  } else {
+    newClass = toggleCollapsedDefaultClass;
+    display = '';
+    text = collapseLiText;
+    color = collapseLiColor;
+  }
+
+  elem.setAttribute('class', newClass);
+  list.style.display = display;
+  elem.textContent = text;
+  elem.style.color = color;
+}
+
+/**
+ * @param {HTMLElement} elem
+ */
+function showRoot(elem) {
+  let target;
+  while (elem) {
+    elem = elem.parentElement || null;
+    if (elem && elem.tagName === 'LI') {
+      target = elem;
+    } else if (elem && elem.getAttribute('class') === commentSectionClass) {
+      elem = null;
+    }
+  }
+  target && target.scrollIntoView(true);
+}
+
+/**
+ * somewhat cpu intensive
+ */
 function addButtons() {
 
   const commentControlsHolders = document.querySelectorAll('.inline-list.inline-list_comment-nav') || [];
 
   for (let i = 0; i < commentControlsHolders.length; i++) {
     const holder = commentControlsHolders[i];
-    const li = document.createElement('li');
-    li.setAttribute('class', defaultClass);
-    li.textContent = collapseLiText;
-    li.style.cursor = 'pointer';
-    li.style.marginTop = '5px';
-    li.style.color = collapseLiColor;
-    holder.appendChild(li);
+
+    const toggleCollapsedBtn = document.createElement('li');
+    toggleCollapsedBtn.setAttribute('class', toggleCollapsedDefaultClass);
+    toggleCollapsedBtn.textContent = collapseLiText;
+    toggleCollapsedBtn.style.cursor = 'pointer';
+    toggleCollapsedBtn.style.marginTop = '5px';
+    toggleCollapsedBtn.style.color = collapseLiColor;
+
+    const showRootBtn = document.createElement('li');
+    showRootBtn.setAttribute('class', showRootClass);
+    showRootBtn.textContent = showRootText;
+    showRootBtn.style.cursor = 'pointer';
+    showRootBtn.style.marginTop = '5px';
+    showRootBtn.style.color = showRoorColor;
+
+    holder.appendChild(toggleCollapsedBtn);
+    holder.appendChild(showRootBtn);
   }
 
 }
